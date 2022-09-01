ed942cd415e6805456158fccc6a0687f6bd65203
ZGZ <301082566@qq.com>
2022-09-01 20:54:37 +0800

    @zgz 标绘

---
+++ src/views-z/011-cesium-bh.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import { defineComponent, onMounted } from 'vue'
+import { Viewer } from 'cesium'
+// development 情况下：解决 vite.config.ts 中 base 路径非根目录时，找不要样式文件的问题
+// produciton  情况下：无此问题
+import '@/../node_modules/cesium/Build/Cesium/Widgets/widgets.css'
+
+onMounted(() => {
+    const viewer = new Viewer('cesiumContainer')
+})
+</script>
+
+<template>
+    <div class="c-0">
+        <div class="title">Cesium demo</div>
+
+        <div id="cesiumContainer"></div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+#cesiumContainer {
+    position: absolute;
+    height: 100%;
+    width: 100%;
+}
+</style>
